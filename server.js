b83577dda900e496d581ff2185f33a5f15132fdb
Anthony Burak DURSUN <badursun@gmail.com>
2021-05-04 14:14:56 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 8080;
 
 corsProxy.createServer({
-    originWhitelist: ['http://localhost:3000', 'https://adjans.com.tr', 'https://nextshippingtr.com'],
+    originWhitelist: ['http://localhost:3000', 'https://adjans.com.tr','https://rabbitcms.adjans.com.tr', 'https://nextshippingtr.com'],
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, () => {
